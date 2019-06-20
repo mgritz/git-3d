d1d04c94a0f3cd2ff8996405518863d603fd5af0
Martin Gritzan <martin.gritzan@gmail.com>
2019-06-20 15:25:44 +0200

    move commit drawing into module

---
+++ yggdrasil.py
@@ -0,0 +1,34 @@
+from OpenGL.GL import *
+
+def draw_diamond(p, size, color=(1,1,1)) :
+    glBegin(GL_TRIANGLE_STRIP)
+    glColor3fv(color)
+    glVertex3f((p[0] + size/2), (p[1] + size/2), p[2])
+    glVertex3f((p[0] - size/2), (p[1] + size/2), p[2])
+    glVertex3f(p[0], p[1], p[2] - size)
+    glVertex3f((p[0] + size/2), (p[1] - size/2), p[2])
+    glVertex3f((p[0] - size/2), (p[1] - size/2), p[2])
+    glVertex3f(p[0], p[1], p[2] + size)
+    glVertex3f((p[0] + size/2), (p[1] + size/2), p[2])
+    glVertex3f((p[0] + size/2), (p[1] - size/2), p[2])
+    glVertex3f(p[0], p[1], p[2] - size)
+    glVertex3f((p[0] - size/2), (p[1] + size/2), p[2])
+    glVertex3f((p[0] - size/2), (p[1] - size/2), p[2])
+    glVertex3f(p[0], p[1], p[2] + size)
+    glEnd()
+
+class YggCommit :
+    def __init__(self, commit) :
+        self.c = commit
+        self.color = (1,1,1)
+        self.position = (0,0,0)
+
+    def __hex2rgb(self, h) :
+        h = h.lstrip('#')
+        return tuple(int(h[i:i+2], 16) / 256 for i in (0, 2, 4))
+
+    def setColorFromString(self, colorstring) :
+        self.color = self.__hex2rgb(colorstring)
+
+    def draw(self) :
+        draw_diamond(self.position, 0.2, self.color)
